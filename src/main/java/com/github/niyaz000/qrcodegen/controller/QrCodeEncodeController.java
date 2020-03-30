82c0203425e5f36acbe5b190bcb319f58e1b2d44
Niyaz K <niyazk2991@gmail.com>
2020-03-30 17:48:11 +0530

    Implement QrCodeDao methods

--- src/main/java/com/github/niyaz000/qrcodegen/controller/QrCodeEncodeController.java
+++ src/main/java/com/github/niyaz000/qrcodegen/controller/QrCodeEncodeController.java
@@ -2,6 +2,8 @@ package com.github.niyaz000.qrcodegen.controller;
 
 import com.github.niyaz000.qrcodegen.constant.EndPoints;
 import com.github.niyaz000.qrcodegen.dto.QrCodeDto;
+import com.github.niyaz000.qrcodegen.service.QrCodeEncodeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,9 @@ import javax.validation.Valid;
 @RequestMapping(EndPoints.ENCODE_ENDPOINT)
 public class QrCodeEncodeController {
 
+  @Autowired
+  QrCodeEncodeService service;
+
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public void post(@Valid QrCodeDto qrCodeDto) {
 
@@ -20,6 +25,11 @@ public class QrCodeEncodeController {
 
   @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public void delete(@PathVariable("id") Long id) {
+
+  }
+
+  @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public void get(@PathVariable("id") Long id) {
 
   }
 
--- src/main/java/com/github/niyaz000/qrcodegen/dao/QrCodeDao.java
+++ src/main/java/com/github/niyaz000/qrcodegen/dao/QrCodeDao.java
@@ -5,18 +5,20 @@ import com.github.niyaz000.qrcodegen.repository.QrCodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class QrCodeDao {
 
   @Autowired
   QrCodeRepository repository;
 
-  public void findById(Long id) {
-
+  public Optional<QrCode> findById(Long id) {
+    return repository.findById(id);
   }
 
   public void deleteById(Long id) {
-
+    repository.deleteById(id);
   }
 
   public QrCode save(QrCode qrCode) {
